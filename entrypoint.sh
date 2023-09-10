9a38af54a582069324246497be3fbd498c20b240
fscarmen2 <fs.carmen@gmail.com>
2023-09-10 23:14:33 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ echo '=============='
 ls /tmp
 
 # 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-base64 -d /tmp/config > /tmp/config.json
+base64 -d config > /tmp/config.json
 UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
 sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g" /tmp/config.json
 
