b561b86c99402138adfd3444c878753d33633268
fscarmen2 <fs.carmen@gmail.com>
2023-09-10 14:11:48 +0000

    Update

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,18 @@
 FROM nginx:latest
+
 EXPOSE 80
-WORKDIR /app
-USER root
+
+WORKDIR /home/choreouser
 
 COPY nginx.conf /etc/nginx/nginx.conf
+
 COPY entrypoint.sh ./
 
-RUN apt-get update && apt-get install -y wget unzip iproute2 systemctl &&\
-    wget -O temp.zip $(wget -qO- "https://api.github.com/repos/v2fly/v2ray-core/releases/latest" | grep -m1 -o "https.*linux-64.*zip") &&\
-    unzip temp.zip v2ray geoip.dat geosite.dat &&\
-    mv v2ray v &&\
-    rm -f temp.zip &&\
+RUN apt-get update && apt-get install -y wget unzip iproute2 systemctl vim netcat-openbsd &&\
+    wget -O /tmp/temp.zip $(wget -qO- "https://api.github.com/repos/v2fly/v2ray-core/releases/latest" | grep -m1 -o "https.*linux-64.*zip") &&\
+    unzip /tmp/temp.zip v2ray geoip.dat geosite.dat -d /tmp/ &&\
+    mv /tmp/v2ray /tmp/v &&\
+    rm -f /tmp/temp.zip &&\
     chmod -v 755 v entrypoint.sh &&\
     echo 'ewogICAgImxvZyI6ewogICAgICAgICJsb2dsZXZlbCI6Indhcm5pbmciLAogICAgICAgICJhY2Nl\
 c3MiOiIvZGV2L251bGwiLAogICAgICAgICJlcnJvciI6Ii9kZXYvbnVsbCIKICAgIH0sCiAgICAi\
@@ -35,6 +37,11 @@ ICAgICAgICB9CiAgICBdLAogICAgIm91dGJvdW5kcyI6WwogICAgICAgIHsKICAgICAgICAgICAg\
 InByb3RvY29sIjoiZnJlZWRvbSIsCiAgICAgICAgICAgICJzZXR0aW5ncyI6ewoKICAgICAgICAg\
 ICAgfQogICAgICAgIH0KICAgIF0sCiAgICAiZG5zIjp7CiAgICAgICAgInNlcnZlcnMiOlsKICAg\
 ICAgICAgICAgIjguOC44LjgiLAogICAgICAgICAgICAiOC44LjQuNCIsCiAgICAgICAgICAgICJs\
-b2NhbGhvc3QiCiAgICAgICAgXQogICAgfQp9Cg==' > config
+b2NhbGhvc3QiCiAgICAgICAgXQogICAgfQp9Cg==' > /tmp/config.json &&\
+    addgroup --gid 10001 choreo &&\
+    adduser --disabled-password  --no-create-home --uid 10001 --ingroup choreo choreouser &&\
+    usermod -aG sudo choreouser
 
 ENTRYPOINT [ "./entrypoint.sh" ]
+
+USER 10001
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,22 +3,25 @@
 # 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
 base64 -d config > config.json
 UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
-VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
-VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
-sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g" config.json
-sed -i "s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g" /etc/nginx/nginx.conf
+sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g" /tmp/config.json
 
 # 伪装 v2ray 执行文件
 RELEASE_RANDOMNESS=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 6)
-mv v ${RELEASE_RANDOMNESS}
-cat config.json | base64 > config
-rm -f config.json
+mv /tmp/v /tmp/${RELEASE_RANDOMNESS}
+cat /tmp/config.json | base64 > /tmp/config
+rm -f /tmp/config.json
 
 # 如果有设置哪吒探针三个变量,会安装。如果不填或者不全,则不会安装
-TLS=${NEZHA_TLS:+'--tls'}
-[ -n "${NEZHA_SERVER}" ] && [ -n "${NEZHA_PORT}" ] && [ -n "${NEZHA_KEY}" ] && wget https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -O nezha.sh && chmod +x nezha.sh && echo '0' | ./nezha.sh install_agent ${NEZHA_SERVER} ${NEZHA_PORT} ${NEZHA_KEY} ${TLS}
+if [ -n "${NEZHA_SERVER}" ] && [ -n "${NEZHA_PORT}" ] && [ -n "${NEZHA_KEY}" ]; then
+  TLS=${NEZHA_TLS:+'--tls'}
+  wget -O /tmp/nezha-agent.zip https://github.com/nezhahq/agent/releases/latest/download/nezha-agent_linux_$(uname -m | sed "s#x86_64#amd64#; s#aarch64#arm64#").zip
+  unzip /tmp/nezha-agent.zip -d /tmp
+  chmod +x /tmp/nezha-agent
+  rm -f /tmp/nezha-agent.zip
+  nohup /tmp/nezha-agent -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${TLS} 2>&1 &
+fi
 
 # 运行 nginx 和 v2ray
 nginx
-base64 -d config > config.json
-./${RELEASE_RANDOMNESS} run
+base64 -d /tmp/config > /tmp/config.json
+/tmp/${RELEASE_RANDOMNESS} run -c /tmp/config.json
